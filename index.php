644346f1a26c8b50809a512fc87f95d5a3415c47
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-12-29 20:41:47 -0500

    index frame and css set

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset=utf-8">
+<title>Kennetic Posts</title>
+<link rel="stylesheet" href="css/styles.css" media="all" />
+</head>
+
+<body>
+    
+    <div class="container">
+    <div>header</div>
+    <div>menu</div>
+        <div>post body</div>
+    <div>sidebar</div>
+    <div>footer</div>
+    </div>
+
+</body>
+</html>
