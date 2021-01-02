75842e505faff34c4c1f977ed89d468b304fcac5
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-02 13:01:04 -0500

    connections for posts made

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html"; charset=utf-8">
+<meta http-equiv="Content-Type" content="text/html"; charset=utf-8>
 <title>Kennetic Posts</title>
 <link rel="stylesheet" href="css/styles.css" media="all" />
 </head>
