30439f564512dcc52b312326038aa48f918e8557
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-05 19:09:09 -0500

    post creation form made

---
+++ admin/insert_post.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>title</title>
+</head>
+
+<body>
+<form action="insert_post.php" method="post" enctype="multipart/form-data">
+    <table width="800" align="center">
+        <tr>
+            <td><h1>Insert New Post</h1></td>
+        </tr>
+        <tr>
+            <td>Post Title:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td>Post Category:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td>Post Author:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td>Post Keywords:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td>Post Image:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td>Post Content:</td>
+            <td><input type="text" name="post_title" /></td>
+        </tr>
+        <tr>
+            <td><input type="submit" name="submit" value="Submit" /></td>
+        </tr>
+    </table>
+</form>
+</body>
+</html>
