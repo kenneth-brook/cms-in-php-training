8030125757860e9eef228feeda90399ccd827d40
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-01-05 19:58:07 -0500

    pulling categories from DB

--- admin/insert_post.php
+++ admin/insert_post.php
@@ -7,36 +7,51 @@
 
 <body>
 <form action="insert_post.php" method="post" enctype="multipart/form-data">
-    <table width="800" align="center">
+    <table width="800" align="center" border="2">
         <tr>
-            <td><h1>Insert New Post</h1></td>
+            <td colspan="6" align="center"><h1>Insert New Post</h1></td>
         </tr>
         <tr>
-            <td>Post Title:</td>
+            <td align="right">Post Title:</td>
             <td><input type="text" name="post_title" /></td>
         </tr>
         <tr>
-            <td>Post Category:</td>
+            <td align="right">Post Category:</td>
+            <td>
+                <select name="cat">
+                    <option>Select A Category</option>
+                    <?php
+                        include("../includes/DB_connect.php");
+                        $get_cats = "SELECT * FROM catagories";
+                        $run_cats = mysqli_query($sql, $get_cats);
+                        while ($cats_row=mysqli_fetch_array($run_cats)){
+                            $cat_id=$cats_row['cat_id'];
+                            $cat_title=$cats_row['cat_title'];
+            
+                            echo "<option value='$cat_id'>$cat_title</option>";
+                        };
+                    ?>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td align="right">Post Author:</td>
             <td><input type="text" name="post_title" /></td>
         </tr>
         <tr>
-            <td>Post Author:</td>
+            <td align="right">Post Keywords:</td>
             <td><input type="text" name="post_title" /></td>
         </tr>
         <tr>
-            <td>Post Keywords:</td>
+            <td align="right">Post Image:</td>
             <td><input type="text" name="post_title" /></td>
         </tr>
         <tr>
-            <td>Post Image:</td>
+            <td align="right">Post Content:</td>
             <td><input type="text" name="post_title" /></td>
         </tr>
         <tr>
-            <td>Post Content:</td>
-            <td><input type="text" name="post_title" /></td>
-        </tr>
-        <tr>
-            <td><input type="submit" name="submit" value="Submit" /></td>
+            <td colspan="6" align="center"><input type="submit" name="submit" value="Submit" /></td>
         </tr>
     </table>
 </form>
